fde54cda2c55398d5baf443b46c6df85d222ee7b
Thomas Horner <thomas@alptruth-geo.com>
2018-09-28 18:22:08 -0600

    add support for FILESERIES models

--- model_processing/get_models.py
+++ model_processing/get_models.py
@@ -23,6 +23,60 @@ def write_to_log (message):
         timewrite = datetime.now().replace(microsecond=0).strftime ("%Y-%m-%d %H:%M:%S")
         f.write ("[" + timewrite + "] " + message + "\n")
 
+def make_grib_filter_url (modelDate, modelHour, fmtTimestep):
+    global model, config
+    gribFilterFilename = model["gribFilename"].replace("%D",modelDate).replace("%H",modelHour).replace("%T",fmtTimestep)
+    gribDirectory = model["gribDirectory"].replace("%D",modelDate).replace("%H",modelHour).replace("%T",fmtTimestep)
+
+    extraParams = ""
+
+    if "variables" in model:
+        for variable in model["variables"]:
+            extraParams += "&var_" + variable + "=on"
+    else:
+        extraParams += "&all_var=on"
+        
+    if "levels" in model:
+        for level in model["levels"]:
+            levelName = level.replace (" ", "_")
+            extraParams += "&lev_" + levelName + "=on"
+    else:
+        extraParams += "&all_lev=on"
+
+    # download every grib file from NOMADS grib filter
+    return (config["downloadTypes"]["GRIBFILTER"]["connectionProtocol"] + 
+        config["downloadTypes"]["GRIBFILTER"]["gribFilterBaseUrl"] + 
+        model["gribFilterName"] +
+        config["downloadTypes"]["GRIBFILTER"]["gribFilterExtension"] + 
+        "file=" + gribFilterFilename +
+        config["downloadTypes"]["GRIBFILTER"]["gribFilterParams"] + 
+        extraParams +
+        "&leftlon=" + config["bounds"]["left"] +
+        "&rightlon=" + config["bounds"]["right"] +
+        "&toplat=" + config["bounds"]["top"] +
+        "&bottomlat=" + config["bounds"]["bottom"] +
+        "&dir=" + gribDirectory
+    )
+
+def make_file_url (modelDate, modelHour, fmtTimestep):
+    global model
+    url = model["url"].replace("%D",modelDate).replace("%H",modelHour).replace("%T",fmtTimestep)
+    return url
+
+def normalize_extents ():
+    global config
+    # return array of [xmin ymin xmax ymax]
+    xmin = float(config["bounds"]["left"])
+    if xmin > 180:
+        xmin = xmin - 360
+    ymin = config ["bounds"]["bottom"]
+    xmax = float(config["bounds"]["right"])
+    if xmax > 180:
+        xmax = xmax - 360
+    ymax = config["bounds"]["top"]
+
+    return [str(xmin), ymin, str(xmax), ymax]
+
 open (directory + '/.get_models_lockfile', 'a').close()
 
 config = data["config"]
@@ -73,18 +127,22 @@ for modelName, model in models.items():
 
         modelDate = modelTime.strftime ("%Y%m%d")
         modelHour = modelTime.strftime ("%H")
-        gribFilename = model["gribFilename"].replace("%D",modelDate).replace("%H",modelHour).replace("%T",str(model["endTime"]))
 
         print "Checking run for this datetime: " + modelDate + " " + modelHour + "Z"
-        fullDirectory = model["baseDirectory"] + model["gribDirectory"].replace("%D", modelDate).replace("%H", modelHour)
-        url = config["connectionProtocol"] + config["nomadsBaseUrl"] + fullDirectory + "/" + gribFilename
-
+        
+        if model["downloadType"] == "GRIBFILTER":
+            gribFilename = model["gribFilename"].replace("%D",modelDate).replace("%H",modelHour).replace("%T",str(model["endTime"]))
+            fullDirectory = model["baseDirectory"] + model["gribDirectory"].replace("%D", modelDate).replace("%H", modelHour)
+            url = config["downloadTypes"]["GRIBFILTER"]["connectionProtocol"] + config["downloadTypes"]["GRIBFILTER"]["nomadsBaseUrl"] + fullDirectory + "/" + gribFilename
+        elif model["downloadType"] == "FILESERIES":
+            url = model["url"].replace("%D",modelDate).replace("%H",modelHour).replace("%T",str(model["endTime"]))
+        
         print "Checking URL: " + url
 
         try:
             ret = urllib2.urlopen(url)
 
-            if ret.code == 200:
+            if ret.code == 200 or ret.code == None:
                 print " *** New model run found. ***"
                 write_to_log (modelName + ": New run found (" + modelDate + modelHour + "Z)")
                 modelsToUpdate[modelName] = model
@@ -127,39 +185,18 @@ for modelName, model in modelsToUpdate.items():
     write_to_log ("Starting update for " + modelName)
 
     for modelTimestep in range (model["startTime"], model["endTime"]+1):
-
         fmtTimestep = str(modelTimestep).rjust (len(str(model["endTime"])), '0')
+        url = ""
+        # for gdalwarp -te extent clip parameter
+        extent = ""
 
-        gribFilterFilename = model["gribFilename"].replace("%D",modelDate).replace("%H",modelHour).replace("%T",fmtTimestep)
-        gribDirectory = model["gribDirectory"].replace("%D",modelDate).replace("%H",modelHour).replace("%T",fmtTimestep)
-
-        extraParams = ""
-
-        if "variables" in model:
-            for variable in model["variables"]:
-                extraParams += "&var_" + variable + "=on"
-        else:
-            extraParams += "&all_var=on"
-            
-        if "levels" in model:
-            for level in model["levels"]:
-                levelName = level.replace (" ", "_")
-                extraParams += "&lev_" + levelName + "=on"
-        else:
-            extraParams += "&all_lev=on"
-
-
-        # download every grib file from NOMADS grib filter
-        url = (config["connectionProtocol"] + config["gribFilterBaseUrl"] + model["gribFilterName"] +
-            config["gribFilterExtension"] + "file=" + gribFilterFilename +
-            config["gribFilterParams"] + 
-            extraParams +
-            "&leftlon=" + config["bounds"]["left"] +
-            "&rightlon=" + config["bounds"]["right"] +
-            "&toplat=" + config["bounds"]["top"] +
-            "&bottomlat=" + config["bounds"]["bottom"] +
-            "&dir=" + gribDirectory)
-
+        if model["downloadType"] == "GRIBFILTER":
+            url = make_grib_filter_url (modelDate, modelHour, fmtTimestep)
+        elif model["downloadType"] == "FILESERIES":
+            url = make_file_url (modelDate, modelHour, fmtTimestep)
+            # for gdalwarp -te extent clip parameter
+            extent = "-te " + " ".join (normalize_extents())
+        
         print "---------------"
         print "Downloading grib file for timestep " + fmtTimestep + "/" + str(model["endTime"]) + "..."
 
@@ -177,13 +214,13 @@ for modelName, model in modelsToUpdate.items():
  
         filename = workingDir + modelName + "_" + modelDate + "_" + modelHour + "Z_f" + fmtTimestep
 
-        with open (filename + ".grib2", 'wb') as outfile:
+        with open (filename + "." + model["filetype"], 'wb') as outfile:
             outfile.write (gribFile.read())
 
         print "Downloaded."
         print ""
         print "Reprojecting and converting to GeoTIFF..."
-        os.system ("gdalwarp " + filename + ".grib2 " + filename + ".vrt -q -t_srs EPSG:4326 -multi --config CENTER_LONG 0")
+        os.system ("gdalwarp " + filename + "." + model["filetype"] + " " + filename + ".vrt -q -t_srs EPSG:4326 " + extent + " -multi --config CENTER_LONG 0")
         os.system ("gdal_translate -co compress=lzw " + filename + ".vrt " + filename + ".tif")
         print "Filesize: " + str(os.path.getsize(filename + ".tif") * 0.000001) + "MB."
         print ""
