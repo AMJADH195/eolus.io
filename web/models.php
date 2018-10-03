c9f616bd6a567d5a88850493883c3cfa30c16e26
thomas.horner@spheraware.com <thomas.horner@spheraware.com>
2018-10-03 13:51:59 -0600

    add nbm

--- web/models.php
+++ web/models.php
@@ -39,6 +39,9 @@ $models = array (
     "nam3" => array (
         "enabled" => true
     ),
+    "nbm" => array (
+        "enabled" => true
+    ),
     "sref_arw_ctl" => array (
         "enabled" => true
     ),
