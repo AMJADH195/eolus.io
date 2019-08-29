01876b4588f2a3fc168a8219c0c901924e427881
Thomas Horner <haevitetty@gmail.com>
2019-08-28 23:37:12 -0600

    add contact

--- web/index.php
+++ web/index.php
@@ -1,3 +1,4 @@
 <?php require ("./eolusHeader.php"); ?>
 <h3>For development purposes only.</h3>
+Have experience with mountain meteorology, angular frontends, cloud administration, or FOSS4G? I could use your help. My email is below.
 <?php require ("./eolusFooter.php"); ?>
