fea8eac3966c368d257db8ce7e326c29329464d8
Thomas Horner <haevitetty@gmail.com>
2019-08-29 11:20:08 -0600

    fix syntax

--- model_processing/get_models.py
+++ model_processing/get_models.py
@@ -612,11 +612,11 @@ for model_timestep in range (model["startTime"], model_loop_end_time):
     try:
         if not ignoredb:
             progress = None;
-            if model_loop_end_time === 1:
+            if model_loop_end_time == 1:
                 progress = 100
             else:
                 progress = (float(model_timestep)/float(model_loop_end_time - 1))*100
-                
+
             curr.execute ("UPDATE logging.model_status SET (progress) = (%s) WHERE model = %s", progress, model_name))
             conn.commit ()
             curr.execute ('UPDATE logging.run_status SET (fh_complete) = (%s) WHERE model = %s AND model_timestamp = %s', (model_timestep, model_name, model_time))
