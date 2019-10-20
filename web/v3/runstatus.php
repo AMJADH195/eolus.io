a0962015d97d27f4e4424facd9bc7ea4b745a2b0
Thomas Horner <haevitetty@gmail.com>
2019-10-19 21:45:09 -0600

    add runstatus endpoint

---
+++ web/v3/runstatus.php
@@ -0,0 +1,23 @@
+<?php
+require ("../../pgcredentials.php");
+header('Content-type: application/json');
+
+$modelStatus = array();
+$error = "";
+
+$conn = pg_connect("host={$pghost} port=5432 dbname={$pgdb} user={$pguser} password={$pgpass} sslmode=require");
+if (!$conn) {
+    $error = "Could not connect to database.";
+} else {
+    $runsResult = pg_query($conn, "SELECT * FROM eolus3.run_status ORDER BY model ASC");
+    while ($row = pg_fetch_assoc($runsResult)) {
+        array_push ($modelStatus, $row);
+    }
+}
+
+echo json_encode (array(
+    "runs" => $modelStatus,
+    "error" => $error
+));
+
+?>
